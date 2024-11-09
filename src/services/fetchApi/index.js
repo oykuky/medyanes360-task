6f4c1bd3cf67ccef75181113a11bd62f33bf0a08
Öykü Kaya <oykuky001@gmail.com>
2024-11-10 01:57:09 +0300

    .

--- src/services/fetchApi/index.js
+++ src/services/fetchApi/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://medyanes360-task-eight.vercel.app/api"|| "http://localhost:3000/api";
+const API_BASE_URL = "https://medyanes360-task-eight.vercel.app" || "http://localhost:3000/api";
 
 const postAPI = async (
   URL,
